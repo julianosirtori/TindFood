6808e613ef91bbc2096b3145e2e539d3e63d97cf
Juliano Sirtori <julianosirtori@gmail.com>
2020-03-08 17:23:50 -0300

    Criado page Cards

--- src/pages/Cards/index.js
+++ src/pages/Cards/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { FaHeart } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md';
 
-import { Container } from './styles';
+import TinderLogo from '../../assets/tinde_logo.svg';
+
+import {
+  Container, ContainerCenter, Logo, Card, Buttons, Button,
+} from './styles';
 
 export default function Cards() {
   return (
     <Container>
-      <h1>Cards</h1>
+      <ContainerCenter>
+        <Logo>
+          <img src={TinderLogo} alt="Tinder Logo" />
+          <h1>TinderFood</h1>
+        </Logo>
+        <Card />
+        <Buttons>
+          <Button>
+            <MdClose color="#FE6C6B" size={35} />
+          </Button>
+          <Button>
+            <FaHeart color="#4FCD91" size={35} />
+          </Button>
+        </Buttons>
+      </ContainerCenter>
     </Container>
   );
 }
--- src/pages/Cards/styles.js
+++ src/pages/Cards/styles.js
@@ -2,4 +2,65 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  height: 100%;
+  background: #F5F7FA;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+`;
+export const ContainerCenter = styled.div`
+  width: 100%;
+  max-width: 374px;
+
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 16px;
+  display: flex;
+  align-items: center;
+
+  img{
+    width: 60px;
+    margin-right: 16px;
+  }
+
+  h1{
+    font-size: 52px;
+    color: #FD3A71;
+    font-weight: bolder;
+  }
+
+`;
+
+export const Card = styled.div`
+  max-width: 375px;
+  width: 100%;
+  height:  567px;
+  box-shadow: 0px 3px 10px #00000029;
+  border-radius: 8px;
+  margin-bottom: 16px;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 64px;
+  padding-right: 64px;
+`;
+
+export const Button = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 70px;
+  width: 70px;
+  background: #FFFFFF;
+  box-shadow: 0px 3px 10px #00000029;
+  border-radius: 48px;
 `;
